f7a9f58269fb0fe93965fd24a6a378897732ae2a
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2023-03-03 11:17:09 +0200

    mergeSort implementation

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "ArrayGenerator.h"
+#include "ArraySorter.h"
 
 int main() {
 
 
     std::vector<long long> v = ArrayGenerator::numberArray(1000, 0, 1000);
+    ArraySorter::mergeSort(v);
     ArrayGenerator::printArray(v);
+
 
 
     return 0;
